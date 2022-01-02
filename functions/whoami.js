ab55b51f2c241ae25d4bb330fdd8a9ad118f1370
KyleSorenson <92221089+KyleSorenson@users.noreply.github.com>
2022-01-02 13:24:56 -0700

    cors test 5

--- functions/whoami.js
+++ functions/whoami.js
@@ -6,19 +6,19 @@ const serverless = require("serverless-http");
 const handler = serverless(app);
 
 module.exports.handler = async (event, context) => {
-  console.log(
-    `{\ncontext: ${JSON.stringify(context, null, 2)},\nevent: ${JSON.stringify(
-      event,
-      null,
-      2
-    )}\n}`
-  );
+  // console.log(
+  //   `{\ncontext: ${JSON.stringify(context, null, 2)},\nevent: ${JSON.stringify(
+  //     event,
+  //     null,
+  //     2
+  //   )}\n}`
+  // );
   const result = await handler(event, context);
+  // result.body is returned
+  console.log(result);
   return {
     statusCode: 200,
-    // headers: {
-    //   "Access-Control-Allow-Origin": "*",
-    // },
+    headers: JSON.stringify(result.headers),
     body: JSON.stringify({
       ipaddress: event.headers["client-ip"],
       language: event.headers["accept-language"],
