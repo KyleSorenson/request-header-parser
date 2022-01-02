144c9c67ae701d6f397bc73d5378c64c2b9d81c2
KyleSorenson <92221089+KyleSorenson@users.noreply.github.com>
2022-01-02 13:39:30 -0700

    cors test v7

--- functions/whoami.js
+++ functions/whoami.js
@@ -14,18 +14,19 @@ module.exports.handler = async (event, context) => {
   //   )}\n}`
   // );
   const result = await handler(event, context);
-  // result.body is returned
-  // console.log(result);
-  return result.body;
-  // return {
-  //   statusCode: 200,
-  //   headers: JSON.stringify(result.headers),
-  //   body: JSON.stringify({
-  //     ipaddress: event.headers["client-ip"],
-  //     language: event.headers["accept-language"],
-  //     software: event.headers["user-agent"],
-  //   }),
-  // };
+  // result object includes statusCode, headers, body
+  console.log(result);
+  // return result;
+  return {
+    statusCode: 200,
+    headers: result.headers,
+    // body: result.body,
+    body: JSON.stringify({
+      ipaddress: event.headers["client-ip"],
+      language: event.headers["accept-language"],
+      software: event.headers["user-agent"],
+    }),
+  };
 };
 
 // module.exports.handler = async (event, context) => {
