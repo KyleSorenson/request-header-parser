878443c19e8bd4ab49bb94ee4e9b558ef3d8df10
KyleSorenson <92221089+KyleSorenson@users.noreply.github.com>
2022-01-01 23:29:45 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,11 +5,22 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require("cors");
+const { Router } = require("express");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+// Parse Header
+const apiRouter = express.Router();
+
+apiRouter.get("/whoami", function (req, res) {
+  console.log(req.headers);
+  // console.log(req.accepted)
+  res.json({
+    ipaddress: req.ip,
+    language: req.header("accept-language"),
+    software: req.header("user-agent"),
+  });
 });
+
+app.use("/api", apiRouter);
 
 module.exports = app;
